051506f5885c9985d8b71868ac48fe18fdba5b11
Chris Stankus <chrisstankus99@gmail.com>
2020-01-29 13:46:56 -0700

    Add end time method

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,14 @@ class School
     @hours_in_school_day = hours_in_school_day
     @start_time = start_time
     @student_names = []
-    @end_time = start_time + hours_in_school_day.to_s
   end
 
    def add_student_name(name)
      @student_names << name
+   end
+
+   def end_time
+    total_hours = start_time.to_i + hours_in_school_day
+    total_hours.to_s + ":00"
    end
 end
